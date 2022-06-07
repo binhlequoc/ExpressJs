8018b718bc9b0ff077430950a0813138cc46a695
BinhLQ <leb553291@gmail.com>
2022-06-08 00:24:38 +0700

    update-pagination

--- src/controllers/AlbumController.js
+++ src/controllers/AlbumController.js
@@ -2,11 +2,16 @@ const AlbumModel = require("../model/AlbumModel");
 const fs = require('fs');
 const { viewsPath } = require("../config/Path.js");
 const { model } = require("mongoose");
-
+const POST_PER_PAGE = 20;
 module.exports = {
     getAlbums: async (req, res) => {
-        const albums = await AlbumModel.find();
-        res.render(viewsPath + "albums", { albums, user: req.user, });
+        let page = Number(req.query.page);
+        if (!page) page = 1;
+        let skip = (page - 1) * 20;
+        const albums = await AlbumModel.find({ user: req.user._id }).skip(skip).limit(POST_PER_PAGE);
+        const count = await AlbumModel.count({ user: req.user._id });
+        const numberAlbum = Math.ceil(count / POST_PER_PAGE);
+        res.render(viewsPath + "albums", { albums, user: req.user, numberAlbum });
     },
     getAddAlbum: async (req, res) => {
 
--- src/controllers/FeedController.js
+++ src/controllers/FeedController.js
@@ -4,15 +4,22 @@ const fs = require('fs');
 const { viewsPath } = require("../config/Path.js");
 const { redirect } = require("express/lib/response");
 const date = require('date-and-time');
+const POST_PER_PAGE = 20;
 module.exports = {
     getFeeds: async (req, res) => {
+        const filter = req.query.filter;
+        let page = Number(req.query.page);
+        if (!page) page = 1;
+        let skip = (page - 1) * 20;
 
-        if (!req.query.filter) {
+        if (!filter) {
             res.redirect("?filter=photos");
         }
-        if (req.query.filter === "photos") {
-            const photos = await PhotoModel.find({ isPublic: true }).populate("user");
+        if (filter === "photos") {
+            const photos = await PhotoModel.find({ isPublic: true }).skip(skip).limit(POST_PER_PAGE).populate("user");
 
+            const count = await PhotoModel.count({ isPublic: true });
+            const numberPhoto = Math.ceil(count / POST_PER_PAGE);
             photos.forEach((value, index) => {
 
                 value.date = date.format(value.createdAt, "h:mm A DD/MM/YYYY");
@@ -21,10 +28,13 @@ module.exports = {
                 button: "photos",
                 photos,
                 user: req.user,
+                numberPhoto,
             });
         }
-        if (req.query.filter === "albums") {
-            const albums = await AlbumModel.find({ isPublic: true }).populate("user");
+        if (filter === "albums") {
+            const albums = await AlbumModel.find({ isPublic: true }).skip(skip).limit(POST_PER_PAGE).populate("user");
+            const count = await AlbumModel.count({ isPublic: true });
+            const numberAlbum = Math.ceil(count / POST_PER_PAGE);
             albums.forEach((value, index) => {
                 value.date = new Date(value.createdAt).toDateString();
             })
@@ -32,6 +42,7 @@ module.exports = {
                 button: "albums",
                 albums,
                 user: req.user,
+                numberAlbum,
             });
         }
 
